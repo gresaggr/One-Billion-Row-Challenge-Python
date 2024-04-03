fa30c4b74595684c9fb228e5ccb99b715ed05b24
gresaggr <sergeyzhirenkin@gmail.com>
2024-04-03 23:06:05 +0300

    solution

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+import numpy
+from numpy import mean
+
 from logger import logger
 import time
+from collections import defaultdict
+
+
 class Solution(object):
     @staticmethod
     def solve():
-        pass
+        meteo_dict = defaultdict(list)
+        fn = r'weather_stations.csv'
+        with open(fn, 'r', encoding='utf-8') as file:
+            for val in file:
+                try:
+                    meteo_name, _, val_s = val.partition(';')
+                    val_f = float(val_s.strip())
+                    meteo_dict[meteo_name].append(val_f)
+                except:
+                    pass
+
+        meteo_dict = {k: meteo_dict[k] for k in sorted(meteo_dict)}
+        for meteo_name, meteo_list in meteo_dict.items():
+            # TODO: нужно ли округлять значения?
+            # print(f'{meteo_name}: {mean(meteo_list)}, {min(meteo_list)}, {max(meteo_list)}')
+            # print(f'{meteo_name}: {mean(numpy.array(meteo_list))}, {min(meteo_list)}, {max(meteo_list)}')
+            print(f'{meteo_name}: {sum(meteo_list) / len(meteo_list)}, {min(meteo_list)}, {max(meteo_list)}')
 
 
 def main():
